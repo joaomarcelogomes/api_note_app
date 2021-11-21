551453dfae0a1b04328283e5e330a26663b97162
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-21 17:59:42 -0300

    addition of bcrypt and hashing password method

--- api/models/user.js
+++ api/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bCrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name: String,
@@ -10,6 +11,21 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     created_at: {type: Date, default: Date.now},
     updated_at: {type: Date, default: Date.now},
-})
+});
+
+userSchema.pre('save', function (next) {
+    if(this.isNew || this.isModified('password')) {
+        const document = this;
+        bCrypt.hash(this.password, 10, 
+            (err, hashedPassword) => {
+                if(err)
+                    next(err)
+                else{
+                    this.password = hashedPassword;
+                    next();
+                }
+            })
+    }
+});
 
 module.exports = mongoose.model('User', userSchema);
