27f01a64101bcced735ad5a643f7523d547d0792
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-22 20:53:49 -0300

    insertion of the PUT and DELETE methods in note routers

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -3,41 +3,77 @@ var router = express.Router();
 const Note = require('../models/note');
 const withAuth = require('../middlewares/auth');
 
-router.post('/', withAuth, async (req,res) => {
+router.post('/', withAuth, async (req, res) => {
     const { title, body } = req.body;
     try {
-        const note = await new Note ({ title, body, author: req.user._id});
+        const note = await new Note({ title, body, author: req.user._id });
         await note.save();
         res.status(201).json(note)
     } catch (error) {
-        res.status(500).json({error: 'Note create failed.'});
+        res.status(500).json({ error: 'Note create failed.' });
     }
 });
 
-router.get('/:id', withAuth, async (req,res) => {
+router.get('/:id', withAuth, async (req, res) => {
     try {
         const { id } = req.params;
         let note = await Note.findById(id);
-        if(isOwner(req.user, note))
+        if (isOwner(req.user, note))
             res.json(note);
         else
-        res.status(403).json({error: 'Unauthorized access'});
+            res.status(403).json({ error: 'Unauthorized access' });
     } catch (error) {
-        res.status(500).json({error: 'Failed to get a note.'});
+        res.status(500).json({ error: 'Failed to get a note.' });
     }
 });
 
-router.get('/', withAuth, async (req,res) =>{
-    try { 
-        const notes = await Note.find({author: req.user._id});
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const notes = await Note.find({ author: req.user._id });
         res.json(notes);
-    } catch(error) {
-        res.json({error: error}).status(500)
+    } catch (error) {
+        res.json({ error: error }).status(500)
     }
-})
+});
+
+router.put('/:id', withAuth, async (req, res) => {
+    const { id } = req.params;
+    const { title, body } = req.body;
+
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            let noteUpdate = await Note.findOneAndUpdate(id,
+                { $set: { title: title, body: body } },
+                { upsert: true, 'new': true }
+            );
+
+            res.json(noteUpdate);
+        } else {
+            res.status(403).json({ error: 'Permission denied' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to update' })
+    }
+});
+
+router.delete('/:id', withAuth, async (req, res) => {
+    const { id } = req.params;
+    try {
+        let note = await Note.findById(id);
+        if (isOwner(req.user, note)) {
+            await note.delete();
+            res.status(200).json({ message: 'Deleted successfully'})
+        } else {
+            res.status(403).json({ error: 'Permission denied' });
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Delete failed' });
+    }
+});
 
 const isOwner = (user, note) => {
-    if(JSON.stringify(user._id) === JSON.stringify(note.author._id))
+    if (JSON.stringify(user._id) === JSON.stringify(note.author._id))
         return true;
     else
         return false;
