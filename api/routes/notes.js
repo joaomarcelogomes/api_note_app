091f573ce7e1073baafea99c6b57ff6a4210a6fe
João Marcelo Gomes <joaomarcelosgomes@gmail.com>
2021-11-22 21:04:23 -0300

    insertion of query search in notes router

--- api/routes/notes.js
+++ api/routes/notes.js
@@ -14,6 +14,18 @@ router.post('/', withAuth, async (req, res) => {
     }
 });
 
+router.get('/search', withAuth, async(req,res) =>{
+    const { query } = req.query;
+    try {
+        let notes = await Note
+            .find({ author: req.user._id })
+            .find({ $text: {$search:query} });
+        res.json(notes);
+    } catch (error) {
+        res.status(500).json({error: error})
+    }
+})
+
 router.get('/:id', withAuth, async (req, res) => {
     try {
         const { id } = req.params;
